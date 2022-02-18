3f75c1682622832690375a41973243c011242342
Yura-Voloshyn <94964569+Yura-Voloshyn@users.noreply.github.com>
2022-02-18 09:54:56 +0000

    Deploying to gh-pages from @ Yura-Voloshyn/group-project-18@82456bb51f42248215739bd20d5970ac29f8ac75 🚀

--- src.5bab00d6.js
+++ src.5bab00d6.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/group-project-18/src.934ffe39.js.map
+//# sourceMappingURL=/group-project-18/src.5bab00d6.js.map
